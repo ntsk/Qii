facec0d5b30a15a23462278ca3d85065d7bd058c
ntsk <ridha.h2a26@gmail.com>
2018-10-07 18:39:33 +0900

    Replace DrawerMenu to BottomNavigation

--- app/src/main/java/com/qii/ntsk/qii/FavoritesFragment.kt
+++ app/src/main/java/com/qii/ntsk/qii/FavoritesFragment.kt
@@ -1,10 +1,10 @@
 package com.qii.ntsk.qii
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class FavoritesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
