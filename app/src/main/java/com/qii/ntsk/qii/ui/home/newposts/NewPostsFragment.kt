ea800906fd001dbcdb3df944ec87399bd9697f71
ntsk <ridha.h2a26@gmail.com>
2019-11-17 23:51:52 +0900

    Update dependencies

--- app/src/main/java/com/qii/ntsk/qii/ui/home/newposts/NewPostsFragment.kt
+++ app/src/main/java/com/qii/ntsk/qii/ui/home/newposts/NewPostsFragment.kt
@@ -6,14 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.qii.ntsk.qii.R
 import com.qii.ntsk.qii.databinding.FragmentNewPostsBinding
 
 class NewPostsFragment : Fragment() {
-    private val viewModel by lazy { ViewModelProviders.of(this).get(NewPostsViewModel::class.java) }
+    private val viewModel by lazy { ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application).create(NewPostsViewModel::class.java) }
     private val controller = NewPostsController()
     private lateinit var binding: FragmentNewPostsBinding
 
