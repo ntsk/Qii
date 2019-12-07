d129b07521b9f64883f90162bc54b21d0ab868d0
ntsk <ridha.h2a26@gmail.com>
2019-12-08 00:01:35 +0900

    Add SearchBottomSheetFragment

---
+++ app/src/main/java/com/qii/ntsk/qii/ui/search/SearchBottomSheetFragment.kt
@@ -0,0 +1,55 @@
+package com.qii.ntsk.qii.ui.search
+
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.qii.ntsk.qii.R
+import com.qii.ntsk.qii.databinding.FragmentSearchBottomSheetBinding
+import com.qii.ntsk.qii.model.entity.Tags
+
+class SearchBottomSheetFragment : BottomSheetDialogFragment() {
+    override fun setupDialog(dialog: Dialog, style: Int) {
+        val view = LayoutInflater.from(requireContext()).inflate(R.layout.fragment_search_bottom_sheet, null, false)
+        val binding = FragmentSearchBottomSheetBinding.bind(view)
+        view.viewTreeObserver.addOnGlobalLayoutListener {
+            val behavior = BottomSheetBehavior.from(view.parent as View)
+            //val margin = 56 * resources.displayMetrics.density.toInt()
+            behavior.peekHeight = view.height
+        }
+
+        val controller = SearchBottomSheetController()
+        binding.fragmentSearchBottomSheetRecyclerView.setController(controller)
+        binding.fragmentSearchBottomSheetRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        arguments?.get(BUNDLE_KEY_SEARCH_BOTTOM_SHEET).let {
+            val arg = it as Tags
+            controller.tags = arg.tags
+            controller.requestModelBuild()
+        }
+
+
+        dialog.setContentView(view)
+    }
+
+    class Builder(tags: Tags) {
+
+        private val args: Bundle = Bundle()
+
+        init {
+            args.putSerializable(BUNDLE_KEY_SEARCH_BOTTOM_SHEET, tags)
+        }
+
+        fun build(): SearchBottomSheetFragment {
+            return SearchBottomSheetFragment().also { it.arguments = args }
+        }
+    }
+
+    companion object {
+        const val BUNDLE_KEY_SEARCH_BOTTOM_SHEET = "search_bottom_sheet"
+    }
+}
--- app/src/main/java/com/qii/ntsk/qii/ui/search/SearchFragment.kt
+++ app/src/main/java/com/qii/ntsk/qii/ui/search/SearchFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.qii.ntsk.qii.R
 import com.qii.ntsk.qii.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    private val viewModel by lazy { ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application).create(SearchViewModel::class.java) }
     private lateinit var binding : FragmentSearchBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -16,5 +19,15 @@ class SearchFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
         binding = FragmentSearchBinding.bind(view)
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.tagsLiveData.observe(viewLifecycleOwner, Observer {tags ->
+            binding.fragmentSearchPostsFab.setOnClickListener{
+                val bottomSheet = SearchBottomSheetFragment.Builder(tags).build()
+                bottomSheet.show(requireFragmentManager(), bottomSheet.tag)
+            }
+        })
     }
 }
