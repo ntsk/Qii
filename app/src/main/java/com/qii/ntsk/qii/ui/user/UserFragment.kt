91f1a05290ae57bfdb61ff367f546256592ed437
ntsk <ridha.h2a26@gmail.com>
2019-12-30 14:20:42 +0900

    Show authorize page when login button clicked

--- app/src/main/java/com/qii/ntsk/qii/ui/user/UserFragment.kt
+++ app/src/main/java/com/qii/ntsk/qii/ui/user/UserFragment.kt
@@ -1,12 +1,16 @@
-package com.qii.ntsk.qii.user
+package com.qii.ntsk.qii.ui.user
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.qii.ntsk.qii.BuildConfig
 import com.qii.ntsk.qii.R
 import com.qii.ntsk.qii.databinding.FragmentUserBinding
+import com.qii.ntsk.qii.utils.RandomStringGenerator
 
 class UserFragment : Fragment() {
     private lateinit var binding: FragmentUserBinding
@@ -19,5 +23,14 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentUserBinding.bind(view)
+        binding.fragmentUserButtonLogin.setOnClickListener {
+            val clientId = BuildConfig.CLIENT_ID
+            val scope = "read_qiita"
+            val state = RandomStringGenerator.generate(40)
+
+            val uri = "https://qiita.com/api/v2/oauth/authorize?client_id=$clientId&scope=$scope&state=$state"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            startActivity(intent)
+        }
     }
 }
