487e28fde026ab5f3d1a7d0526f5ca690a1030a6
ntsk <ridha.h2a26@gmail.com>
2019-12-30 15:59:07 +0900

    Add TokenHolder

--- app/src/main/java/com/qii/ntsk/qii/model/TokenHolder.kt
+++ app/src/main/java/com/qii/ntsk/qii/model/TokenHolder.kt
@@ -1,4 +1,20 @@
 package com.qii.ntsk.qii.model
 
-class TokenHolder {
+import android.content.Context
+
+class TokenHolder(context: Context) {
+    private val sharedPreference = context.getSharedPreferences(TOKEN_PREF, Context.MODE_PRIVATE)
+
+    companion object {
+        private const val TOKEN_PREF = "token_pref"
+        private const val TOKEN_PREF_KEY = "token_pref_key"
+    }
+
+    fun save(token: String) {
+        sharedPreference.edit().putString(TOKEN_PREF_KEY, token).apply()
+    }
+
+    fun load(): String? {
+        return sharedPreference.getString(TOKEN_PREF_KEY, null)
+    }
 }
