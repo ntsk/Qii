6c09d0c91d01be4e1d46b03211e2bbd88c290cb0
ntsk <ridha.h2a26@gmail.com>
2019-12-30 16:13:52 +0900

    Show message login is success or fail

--- app/src/main/java/com/qii/ntsk/qii/ui/MainActivity.kt
+++ app/src/main/java/com/qii/ntsk/qii/ui/MainActivity.kt
@@ -1,17 +1,19 @@
-package com.qii.ntsk.qii
+package com.qii.ntsk.qii.ui
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.qii.ntsk.qii.R
 import com.qii.ntsk.qii.databinding.ActivityMainBinding
 import com.qii.ntsk.qii.favorite.FavoritesFragment
-import com.qii.ntsk.qii.ui.MainViewModel
+import com.qii.ntsk.qii.model.TokenHolder
 import com.qii.ntsk.qii.ui.home.HomeFragment
 import com.qii.ntsk.qii.ui.search.SearchFragment
 import com.qii.ntsk.qii.ui.user.UserFragment
@@ -19,6 +21,7 @@ import com.qii.ntsk.qii.ui.user.UserFragment
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val FRAGMENT_ID = R.id.fragment_container
+        private const val AUTHORIZE_CALLBACK_URI = "qii://callback"
     }
 
     private val viewModel by lazy { ViewModelProvider.AndroidViewModelFactory.getInstance(this.application).create(MainViewModel::class.java) }
@@ -69,16 +72,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onNewIntent(intent: Intent?) {
-
         val uri = intent?.data
-        if (uri != null) {
-            val code = uri.getQueryParameter("code") ?: return
-            viewModel.getToken(code).observe(this, Observer {
-
-            })
+        if (uri != null && uri.toString().contains(AUTHORIZE_CALLBACK_URI)) {
+            authorize(uri)
         }
 
         super.onNewIntent(intent)
+    }
+
+    private fun authorize(uri: Uri) {
+        val code = uri.getQueryParameter("code") ?: return
+        viewModel.getToken(code).observe(this, Observer {
+            val tokenHolder = TokenHolder(this)
+            tokenHolder.save(it.token)
+            Toast.makeText(this, R.string.message_success_login, Toast.LENGTH_LONG).show()
+        })
+
+        viewModel.errorObserver.observe(this, Observer {
+            Toast.makeText(this, R.string.message_fail_login, Toast.LENGTH_LONG).show()
+        })
     }
 
     private fun <F> setDefaultFragment(fragment: F) where F : Fragment {
