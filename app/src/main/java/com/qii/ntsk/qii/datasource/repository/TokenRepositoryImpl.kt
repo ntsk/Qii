7f3c3b7e207ba6eabcc5b5bcfb7833cf6661a6b2
ntsk <ridha.h2a26@gmail.com>
2020-08-03 01:04:18 +0900

    Add TokenModule

---
+++ app/src/main/java/com/qii/ntsk/qii/datasource/repository/TokenRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.qii.ntsk.qii.datasource.repository
+
+import com.qii.ntsk.qii.BuildConfig
+import com.qii.ntsk.qii.model.entity.Token
+import com.qii.ntsk.qii.datasource.service.ApiClient
+import com.qii.ntsk.qii.datasource.service.QiitaService
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TokenRepositoryImpl @Inject constructor() : TokenRepository {
+    override suspend fun getToken(code: String): Response<Token> {
+        return ApiClient.create(QiitaService::class.java).getToken(
+                code = code,
+                clientId = BuildConfig.CLIENT_ID,
+                clientSecret = BuildConfig.CLIENT_SECRET
+        )
+    }
+
+    override suspend fun revokeToken(token: String): Response<Void> {
+        return ApiClient.create(QiitaService::class.java).revokeToken(
+                token = token
+        )
+    }
+}
